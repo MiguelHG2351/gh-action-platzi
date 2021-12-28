be19617b0387959c22d3a28e11daeb0c662a56e9
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-12-28 05:49:59 -0600

    url gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://miguelhg2351.github.io/',
+  base: 'https://miguelhg2351.github.io/gh-action-platzi/',
   plugins: [react()]
 })
